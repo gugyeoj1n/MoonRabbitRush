ca91f7c36514a8b5bc588257f965679bd03e58f3
Woojin Kwak <kwooj2788@kookmin.ac.kr>
2026-07-26 05:09:58 +0900

    #9 [곽우진] 웨이브 데이터 구조 구현

--- Assets/Scripts/Runtime/Waves/IWaveDirector.cs
+++ Assets/Scripts/Runtime/Waves/IWaveDirector.cs
@@ -4,6 +4,8 @@ namespace MoonRabbitRush.Waves
     {
         int CurrentWave { get; }
         bool IsRunning { get; }
+        float ElapsedTime { get; }
+        float RemainingTime { get; }
         void StartNextWave();
         void Stop();
     }
