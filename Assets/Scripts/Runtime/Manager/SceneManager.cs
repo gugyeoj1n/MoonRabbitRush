73570edf0ae8fb01f529032df934cc16b11ca08b
TYDTYD <48386074+TYDTYD@users.noreply.github.com>
2026-07-25 19:33:51 +0900

    #3 [이승민] 기타 파일 추가

--- Assets/Scripts/Runtime/Manager/SceneManager.cs
+++ Assets/Scripts/Runtime/Manager/SceneManager.cs
@@ -1,12 +1,37 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace MoonRabbitRush
-{
+{    
     public class SceneManager : MonoBehaviour
     {
-        Scene ActiveScene;
+        Scene ActiveScene => EditorSceneManager.GetActiveScene();
 
-        
+
+        public async UniTask<bool> TransitionTo(int index)
+        {
+            if (ActiveScene == EditorSceneManager.GetSceneByBuildIndex(index))
+                return false;
+
+            try
+            {
+                
+                await UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(index);
+            }
+            catch(Exception ex)
+            {
+                Debug.LogError($"Scene Transition Error! => {ex}");
+            }
+            finally
+            {
+
+            }
+
+            return true;
+        }
     }
 }
