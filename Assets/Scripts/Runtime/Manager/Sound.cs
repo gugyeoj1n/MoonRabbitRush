d39515cb3ebc647004b6bd8f655f777ba42e3783
TYDTYD <48386074+TYDTYD@users.noreply.github.com>
2026-08-02 18:55:34 +0900

    #22 사운드 매니저 등록
    Sound Enum 정의
    Sound Manager 구현
    SoundManager의 Play를 통해 사운드 재생 가능합니다

---
+++ Assets/Scripts/Runtime/Manager/Sound.cs
@@ -0,0 +1,15 @@
+namespace MoonRabbitRush
+{
+    public enum Sound
+    {
+        None = 0,
+        PlayerHit,
+        PlayerDeath,
+        EnemyHit,
+        EnemyDeath,
+        BossAlert,
+        LevelUp,
+        WeaponFire,
+        UiClick,
+    }
+}
