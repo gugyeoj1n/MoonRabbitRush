477a69c253678e56dd25e8f3ce2c3e5a69a88395
TYDTYD <48386074+TYDTYD@users.noreply.github.com>
2026-08-07 22:07:12 +0900

    #22 배경음 및 SFX 사운드 조절 시스템 구현

--- Assets/Scripts/Runtime/UI/PopupSystemUI.cs
+++ Assets/Scripts/Runtime/UI/PopupSystemUI.cs
@@ -1,12 +1,43 @@
 using Cysharp.Threading.Tasks;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MoonRabbitRush
 {
     public class PopupSystemUI : UIPopup
     {
+        [SerializeField]
+        private Slider _sfxSlider;
+        [SerializeField]
+        private Slider _bgmSlider;
         private CancellationTokenSource _tokenSource;
+        private SoundManager _soundManager => ManagerRoot.Instance.SoundManager;
+
+        private void OnEnable()
+        {
+            _sfxSlider.value = _soundManager.SfxVolume;
+            _bgmSlider.value = _soundManager.BgmVolume;
+
+            _sfxSlider.onValueChanged.AddListener(OnSfxVolumeChanged);
+            _bgmSlider.onValueChanged.AddListener(OnBgmVolumeChanged);
+        }
+
+        private void OnDisable()
+        {
+            _sfxSlider.onValueChanged.RemoveListener(OnSfxVolumeChanged);
+            _bgmSlider.onValueChanged.RemoveListener(OnBgmVolumeChanged);
+        }
+
+        private void OnSfxVolumeChanged(float value)
+        {
+            _soundManager.SfxVolume = value;
+        }
+
+        private void OnBgmVolumeChanged(float value)
+        {
+            _soundManager.BgmVolume = value;
+        }        
         public void OnClickResume()
         {
             OnClickClose();
